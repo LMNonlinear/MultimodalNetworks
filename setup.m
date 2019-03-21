7e63844a80039bbf90f19239b927c1686fc308c6
Rigel Falcon <31567623+rigelfalcon@users.noreply.github.com>
2019-03-21 20:33:11 +0800

    update inverse pipeline

--- setup.m
+++ setup.m
@@ -1,2 +1,12 @@
+restoredefaultpath
 addpath(genpath('F:\MEEGfMRI\Data\ResamplingfMRIpipeline\external'));
-addpath(genpath('.\external'));
+% addpath(genpath('.\external'));
+%%
+addpath('F:\codes\spm12')
+spm
+%% barinstorm
+addpath('F:\codes\brainstorm3')
+brainstorm('nogui')
+%% fieldtrip
+addpath('F:\codes\fieldtrip-20161224')
+ft_defaults
--- signal_comparing_meg_fmri.m
+++ signal_comparing_meg_fmri.m
@@ -1,6 +1,7 @@
-clear;close all;clc;
+% clear;clc;
+close all;
 %% FLAG
-
+FLAG_LOAD_DATA=0;
 FLAG_DISPLAY=1;
 FLAG_DISPLAY_REULT=1;
 %%
@@ -11,47 +12,41 @@ PATH_DATASET='F:\MEEGfMRI\Data\HCP_S900\';
 pipeline_path=mfilename('fullpath');
 [pipeline_path, pipeline_name, ~] = fileparts(pipeline_path);
 
-%% fmri downsampled signal 
-surfSignal(1).path_fmri={'F:\MEEGfMRI\Data\SignalComparing\data\105923.rs.4k.rfMRI_REST1_LR_Atlas_hp2000_clean.L.nii'...
-'F:\MEEGfMRI\Data\SignalComparing\data\105923.rs.4k.rfMRI_REST1_LR_Atlas_hp2000_clean.R.nii'};
+%% fmri downsampled signal
+signal.fmri.path={'.\data\105923.rs.4k.rfMRI_REST1_LR_Atlas_hp2000_clean.L.nii'...
+    '.\data\105923.rs.4k.rfMRI_REST1_LR_Atlas_hp2000_clean.R.nii'};
 
 
-%% fmri raw signal 
-surfSignal(1).path_fmri_raw={'F:\MEEGfMRI\Data\HCP_S900\105923\MNINonLinear\Result\rfMRI_REST1_LR\rfMRI_REST1_LR_Atlas_hp2000_clean.R.nii' ...
-'F:\MEEGfMRI\Data\HCP_S900\105923\MNINonLinear\Result\rfMRI_REST1_LR\rfMRI_REST1_LR_Atlas_hp2000_clean.L.nii'};
+%% MEG raw signal
+signal.meg_raw.path={'F:\MEEGfMRI\Data\HCP_S900\105923\unprocessed\MEG\3-Restin\4D\c,rfDC'};
+signal.meg_raw.header=ft_read_header(signal.meg_raw.path);
+%% MEG filterd
+%% MRI
+signal.mri.path={[PATH_DATASET,NUM_SUBJ,'\MEG\anatomy\',NUM_SUBJ,'.L.midthickness.4k_fs_LR.surf.gii'],...
+    [PATH_DATASET,NUM_SUBJ,'\MEG\anatomy\',NUM_SUBJ,'.R.midthickness.4k_fs_LR.surf.gii']};
+gii.left=gifti(signal.mri.path{1}); %cortical surface 
+gii.right=gifti(signal.mri.path{2});
+signal.mri.faces{1}=gii.left.faces;
+signal.mri.faces{2}=gii.right.faces;
 
-%% MEG raw signal 
-surfSignal(1).path_meg={'F:\MEEGfMRI\Data\HCP_S900\105923\MEG\Restin\icablpenv'};
-surfSignal(1).path_meg=file_find(surfSignal(1).path_meg{1},'*.power.dtseries.nii', 1,0);%brainsotrm
-% for i=1:max(size(surfSignal.path_meg))
+signal.mri.vertices{1}=gii.left.vertices;
+signal.mri.vertices{2}=gii.right.vertices;
 
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %% file_read
-surfSignal(1).signal_fmri=niftiopen(surfSignal(1).path_fmri);
-surfSignal(1).signal_fmri_raw=niftiopen(surfSignal(1).path_fmri_raw);
-for i=1:2
-    surfSignal(1).signal_meg{i}=ft_read_cifti(surfSignal(1).path_meg{i});
+if FLAG_LOAD_DATA==1
+    signal.fmri.signal=niftiopen(signal.fmri.path);
+    signal.meg_raw.signal=ft_read_data(signal.meg_raw.path);
 end
-
-%% correlation
-% corrMarix{1,2}.time=corr(surfSignal(1).signal_fmri{1},surfSignal(2).signal_fmri{1});
-% figure;imagesc(corrMarix{1,2}.time,[-1,1]);
-% figure;plot(diag(corrMarix{1,2}.time));
-% corrMarix{1,2}.channel=corr(surfSignal(1).signal_fmri{1}',surfSignal(2).signal_fmri{1}');
-% figure;imagesc(corrMarix{1,2}.channel,[-1,1]);
-% figure;plot(diag(corrMarix{1,2}.channel));
-%% leastsuqare
-% mseMarix{1,2}.time=mse(surfSignal(1).signal_fmri{1},surfSignal(2).signal_fmri{1});
-% figure;imagesc(mseMarix{1,2}.time,[-1,1]);
-% figure;plot(diag(mseMarix{1,2}.time));
-% mseMarix{1,2}.channel=mse(surfSignal(1).signal_fmri{1}',surfSignal(2).signal_fmri{1}');
-% figure;imagesc(mseMarix{1,2}.channel,[-1,1]);
-% figure;plot(diag(mseMarix{1,2}.channel));
-
-
-
-
-
-
-
+%% %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% figure
+% MEG
+idx=find(signal.meg_raw.header.grad.chanpos(:,3)>0.2);
+plot3(signal.meg_raw.header.grad.chanpos(:,1),signal.meg_raw.header.grad.chanpos(:,2),signal.meg_raw.header.grad.chanpos(:,3),'.')
+hold on
+plot3(signal.meg_raw.header.grad.chanpos(idx,1),signal.meg_raw.header.grad.chanpos(idx,2),signal.meg_raw.header.grad.chanpos(idx,3),'o')
+% MRI
+% plotatlas(gii.left);
+% plotatlas(gii.right);
 
 
