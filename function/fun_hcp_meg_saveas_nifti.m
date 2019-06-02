303a7251c5d8597296b8181a77ded7bd7ad7efe7
rigelfalcon <31567623+rigelfalcon@users.noreply.github.com>
2019-06-02 16:20:58 -0400

    Update fun_hcp_meg_saveas_nifti.m

--- function/fun_hcp_meg_saveas_nifti.m
+++ function/fun_hcp_meg_saveas_nifti.m
@@ -1,20 +1,24 @@
-% function varargout= fun_hcp_meg_saveas_nifti(varargin)
-% 
-% switch nargin
-%     case 0
-%         load .\temp\fun_hcp_meg_inverse_pipeline.mat
-%     case 1
-%         megSignal=varargin{1};
-% end
+function varargout= fun_hcp_meg_saveas_nifti(varargin)
+
+switch nargin
+    case 0
+        load .\temp\fun_hcp_meg_inverse_pipeline.mat
+    case 1
+        load .\temp\config.mat
+        megSignal=varargin{1};
+    case 2
+        SubjectName=varargin{2};
+        megSignal=varargin{1};
+end
+addpath('.\external\nifti-analyze-matlab\');
+
+%% creat nii
+% img=[varargin{1}];
+img=reshape(Results.ImageGridAmp,[size(Results.ImageGridAmp,1),1,1,size(Results.ImageGridAmp,2)]);
+description='Source MEG signal- Create by CCCLAB';
+nii = make_nii(img, [], [], [],[]);
 %%
-load .\temp\hcp_inverse_pipeline.mat
-%%
-niftiObj=nifti;
-file_arrayObj=file_array;
-% niftiObj.dat=Results.ImageGridAmp;
-
-
-
-% end
+save_nii(nii,['.\result\',SubjectName,'.',num2str(round(size(Results.ImageGridAmp,1)/2000)),'k.source.MEG_REST_LR.nii'])
+end
 
 
