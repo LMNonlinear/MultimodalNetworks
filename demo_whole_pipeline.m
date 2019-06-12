d6661eb11da005ba449d6f3f26fb31d36d274d79
rigelfalcon <31567623+rigelfalcon@users.noreply.github.com>
2019-06-12 11:24:03 -0400

    update setup.m

--- demo_whole_pipeline.m
+++ demo_whole_pipeline.m
@@ -14,7 +14,7 @@ megNiftiPath='.\result\105923.4k.source.MEG_REST_LR.nii';
 fmriLabelPath={['.\result\105923.rs.from32k.4k.105923.aparc.32k_fs_LR.L.label.gii'],...
     ['.\result\105923.rs.from32k.4k.105923.aparc.32k_fs_LR.R.label.gii']};
 [pathData]=fun_hcp_match_label(SubjectName,fmriNiftiPath,megNiftiPath,fmriLabelPath,'mat');
-%%
+%% bands and envelope
 [pathMegBand,megBand]= fun_group_in_freqs_bands(SubjectName);
 [pathMegBandEnvelope,megBandEnvelope]= fun_bands_envelope(SubjectName,megBand);
 %%
--- setup.m
+++ setup.m
@@ -1,20 +1,32 @@
 close all;clear;clc;
 restoredefaultpath
-%% barinstorm
-addpath('E:\Rigel\codes\brainstorm3')
-% addpath('D:\codes\brainstorm\brainstorm_190516\brainstorm3')
-addpath('.\function\')
-brainstorm('setpath')
-%% demo parameter
-ProtocolName='HCPMEGPipeline';
-data_dir='E:\Rigel\MEEGfMRI\Data\HCP_S900\';
-% data_dir='M:\MEEGfMRI\Data\HCP_S900\';
-SubjectName = '105923';
-% wb_command='D:\Software\workbench\bin_windows64\wb_command.exe';
-wb_command='E:\Rigel\codes\workbench\bin_windows64\wb_command.exe';
-%% save
-filename = './temp/config.mat';
-save(filename)
-
-
+[~, hostname] = system('hostname');
+switch hostname
+    case 'KBOMATEBOOKXPRO '
+        %% barinstorm
+        addpath('D:\codes\brainstorm\brainstorm_190516\brainstorm3')
+        addpath('.\function\')
+        brainstorm('setpath')
+        %% demo parameter
+        ProtocolName='HCPMEGPipeline';
+        data_dir='M:\MEEGfMRI\Data\HCP_S900\';
+        SubjectName = '105923';
+        wb_command='D:\Software\workbench\bin_windows64\wb_command.exe';
+        %% save
+        filename = './temp/config.mat';
+        save(filename)
+    case 'HPCwin'
+        %% barinstorm
+        addpath('E:\Rigel\codes\brainstorm3')
+        addpath('.\function\')
+        brainstorm('setpath')
+        %% demo parameter
+        ProtocolName='HCPMEGPipeline';
+        data_dir='E:\Rigel\MEEGfMRI\Data\HCP_S900\';
+        SubjectName = '105923';
+        wb_command='E:\Rigel\codes\workbench\bin_windows64\wb_command.exe';
+        %% save
+        filename = './temp/config.mat';
+        save(filename)
+end
 
